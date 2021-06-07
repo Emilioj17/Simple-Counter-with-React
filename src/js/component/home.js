72a7e4cc8a2a350bdb52265985266496f70b199e
Emilio Jelves <ejjelves@gmail.com>
2021-06-07 17:45:45 -0400

    Ultima Version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+let Home = props => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<div
+			className="text-center pt-5 bg-dark text-white"
+			style={{ height: "100vh", fontSize: "100px" }}>
+			<p className="d-inline px-2">
+				<i className="far fa-clock"></i>
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="d-inline">
+				<p className="d-inline digCuatro">{props.cuatro}</p>
+				<p className="d-inline digTres">3</p>
+				<p className="d-inline digDos">2</p>
+				<p className="d-inline digUno">1</p>
+			</div>
 		</div>
 	);
-}
+};
+
+Home.propTypes = {
+	cuatro: PropType.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,7 +10,45 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+//import { Home } from "./component/home.js";
 
+let Home = props => {
+	return (
+		<div
+			className="text-center pt-5 bg-dark text-white"
+			style={{ height: "100vh", fontSize: "100px" }}>
+			<p className="d-inline px-2">
+				<i className="far fa-clock"></i>
+			</p>
+			<div className="d-inline">
+				<p className="d-inline digCuatro">{props.cuatro}</p>
+				<p className="d-inline digTres">{props.tres}</p>
+				<p className="d-inline digDos">{props.dos}</p>
+				<p className="d-inline digUno">{props.uno}</p>
+			</div>
+		</div>
+	);
+};
+
+Home.propTypes = {
+	cuatro: PropTypes.string,
+	tres: PropTypes.string,
+	dos: PropTypes.string,
+	uno: PropTypes.string
+};
+
+//Esta función y lógica no es mia.
+let counter = 0;
+setInterval(function() {
+	const ncuatro = Math.floor(counter / 1000);
+	const ntres = Math.floor(counter / 100);
+	const ndos = Math.floor(counter / 10);
+	const nuno = Math.floor(counter / 1);
+	console.log(ncuatro, ntres, ndos, nuno);
+	counter++;
+	ReactDOM.render(
+		<Home cuatro={ncuatro} tres={ntres} dos={ndos} uno={nuno} />,
+		document.querySelector("#app")
+	);
+}, 1000);
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
